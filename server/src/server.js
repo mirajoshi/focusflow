0e489eaef675992d478538c05c0f5f0490c8c505
Mira Joshi <mirajoshi.1107@gmail.com>
2026-08-08 21:58:03 +0530

    Connect Express server to MongoDB Atlas via Mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,16 @@
 import 'dotenv/config';
 import app from './app.js';
+import connectDB from './config/db.js';
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  });
